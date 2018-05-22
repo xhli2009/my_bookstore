ee9c7aaa178f609236d05b36ed451fa47b36ce30
xhli2009 <xhli2009@163.com>
2018-05-22 18:37:04 +0800

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,12 @@
 {
 	'name':'My BookStore',
 	'summary':"Manage your books",
+	'category':'Library',
 	'depends':['base','decimal_precision'],
 	'data':[
-		'security/groups.xml',
+	#	'security/groups.xml',
+		'security/library_security.xml',
 		'views/library_book.xml',
-		'security/ir.model.access.csv',
+	#	'security/ir.model.access.csv',
 		],
 }
